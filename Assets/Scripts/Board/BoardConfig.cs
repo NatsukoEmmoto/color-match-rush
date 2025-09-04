bf996c63ea339e61bb2116576591c8a67bf4af83
NatsukoEmmoto <natsuko.emmoto@yen-games.com>
2025-09-04 09:44:02 +0900

    chore: add BoardConfig (SO) and BoardLayout skeleton

---
+++ Assets/Scripts/Board/BoardConfig.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace ColorMatchRush
+{
+    [CreateAssetMenu(fileName = "BoardConfig", menuName = "ColorMatchRush/BoardConfig", order = 1)]
+    public class BoardConfig : ScriptableObject
+    {
+        [SerializeField] private int width = 8;
+        [SerializeField] private int height = 8;
+        [SerializeField] private float cellSize = 1f;
+        [SerializeField] private bool autoCenter = true;
+        [SerializeField] private Vector2 explicitOrigin = Vector2.zero;
+        [SerializeField] private int randomSeed = 0;
+        [SerializeField] private bool preventInstantMatchesOnStart = true;
+        [SerializeField] private int maxInstantMatchRegenerations = 10;
+        [SerializeField] private float swapMoveDuration = 0.15f;
+        [SerializeField] private float fallMoveDuration = 0.1f;
+        [SerializeField] private int spawnOvershootCells = 2;
+
+        public int Width => width;
+        public int Height => height;
+        public float CellSize => cellSize;
+        public bool AutoCenter => autoCenter;
+        public Vector2 ExplicitOrigin => explicitOrigin;
+        public int RandomSeed => randomSeed;
+        public bool PreventInstantMatchesOnStart => preventInstantMatchesOnStart;
+        public int MaxInstantMatchRegenerations => maxInstantMatchRegenerations;
+        public float SwapMoveDuration => swapMoveDuration;
+        public float FallMoveDuration => fallMoveDuration;
+        public int SpawnOvershootCells => spawnOvershootCells;
+    }
+}
